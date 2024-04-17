c3ef9e7db5d72cf5ff482bc09747d08639f43656
StormHooper <116904616+StormHooper@users.noreply.github.com>
2024-04-17 15:59:46 -0700

    testing

--- Prog 5 - Bucket/bucketList.cpp
+++ Prog 5 - Bucket/bucketList.cpp
@@ -1,6 +1,6 @@
 // Author: Tyler Pham
 // CECS 325-02
-// Prog 5 - Bucket List (vectors)
+// Prog 5 - Bucket List
 // Due 04/23/2024
 //
 // I certify that this program is my own original work. I did not copy any part of this program from
@@ -12,17 +12,65 @@
 #include <ctime>
 using namespace std;
 
+int globalSwapCount {0};
 class Bucket{
     private:
         vector<int> v;
 
     public:
-        Bucket();
-        void generate(int size, int min, int max); 
-        void sort(); // Use the bubble sort from Prog3 and Prog4
-        int size();
-        int atIndex();
-        int merge(Bucket b); // merge b into this
+        Bucket() {};
+
+        void generate(int size, int min, int max)
+        {
+            for (size_t i {0}; i < size; ++i)
+                v.push_back(rand() % abs(min) - max - 1 + min);
+        }
+
+        void sort() // Use the bubble sort from Prog3 and Prog4
+        {
+            int temp {};
+            for (size_t i {0}; i < size(); ++i)
+                for (size_t j {i+1}; j < size(); ++j)
+                    if (v[i] > v[j])
+                    {
+                        temp = v[i];
+                        v[i] = v[j];
+                        v[j] = temp;
+                        ++globalSwapCount;
+                    }
+        }
+
+        int size()
+        {
+            return v.size();
+        }
+
+        int atIndex(int i)
+        {
+            return v[i];
+        }
+
+        int merge(Bucket b) // merge b into this
+        {
+            int temp[size() + b.size()];
+            size_t i {0}, index1 {0}, index2 {0};
+
+            while (index1 < size() && index2 < b.size()) 
+                temp[i++] = (v[index1] < b.v[index2]) ? v[index1++] : b.v[index2++];
+
+            while (index1 < size())
+                temp[i++] = v[index1++];
+            while (index2 < size())
+                temp[i++] = v[index2++];
+
+            for (size_t i {0}; i < sizeof(temp)/sizeof(temp[i]); ++i)
+            {
+                if (i < size())
+                    v[i] = temp[i];
+                else
+                    v.push_back(temp[i]);
+            }
+        }
 };
 
 int main(int argc, char *argv[])
@@ -33,10 +81,10 @@ int main(int argc, char *argv[])
     int bucketSize = stoi(argv[2]);
     int bucketMin = stoi(argv[3]);
     int bucketMax = stoi(argv[4]);
-    cout << "Bucket Count:"<< bucketCount <<endl;
-    cout << "Bucket Size:"<< bucketSize <<endl;
-    cout << "Bucket Min Value:"<< bucketMin <<endl;
-    cout << "Bucket Max value:"<< bucketMax <<endl;
+    cout << "Bucket Count:" << bucketCount << endl;
+    cout << "Bucket Size:" << bucketSize << endl;
+    cout << "Bucket Min Value:" << bucketMin << endl;
+    cout << "Bucket Max value:" << bucketMax << endl;
 
     vector<Bucket> list; // create empty Bucket vector
     Bucket *bptr;
